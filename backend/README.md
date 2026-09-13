1b06634f7822b136044bd810033841f4235980d6
EMERICK GIBERNE <egiberne@gmail.com>
2026-09-13 04:25:56 +0000

    build local backend api

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,33 @@
 # Chunck 2
 
+
+## Setup
+
+```bash
+
+:: Install Python libraries
+
+cd " backend"
+
+:: Create environment
+python -m venv env    
+
+::: Start environment
+..\env\scripts\activate  
+
+::: Install Web server
+pip install uvicorn
+
+::: Install API
+pip install fastapi
+
+::: Install database bridge
+pip install sqlalchemy
+
+:::: Restart web server
+uvicorn api.main:app --reload
+```
+
 ## 2. Azure Function using the Azure portal
 - Set up the function with [Function Premium](https://learn.microsoft.com/en-us/azure/azure-functions/functions-create-function-app-portal) or other because of the missing edition mode in the portal for Flex-Consumption Plan.
 - Exclude legacy Consumption Plan since Python is unsupported
---
+++ frontend/src/index.js
@@ -0,0 +1,56 @@
+// Load or initialize visit counter
+let visitCounter = parseInt(localStorage.getItem("visit_counter") || "0")
+
+// Load or create a user ID 
+let userId = localStorage.getItem("user_id")
+console.log("Loaded user ID:", userId)
+if (!userId) { // it does not exist then create
+    userId = crypto.randomUUID()
+    localStorage.setItem("user_id", userId)
+    console.log("New user ID created:", userId)
+}
+
+// Increment visit counter and store it
+visitCounter +=1
+localStorage.setItem("visit_counter", visitCounter)
+
+// Create the HTML content
+let html = `
+    It is your #${visitCounter} visit
+`
+// Display the content
+document.getElementById('footer').innerHTML= html
+
+
+fetch("http://127.0.0.1:8000/health")
+.then(response => response.text())
+.then(data=>console.log(data))
+
+fetch("http://127.0.0.1:8000/echo",{method:"POST"})
+.then(response => response.text())
+.then(data => console.log(data))
+
+fetch("http://127.0.0.1:8000/v1/visits",{
+    method:"POST",
+    body:JSON.stringify({userId:userId})
+})
+.then(response => response.json())
+.then(data => console.log(data))
+
+
+fetch("http://127.0.0.1:8000/v2/visits",{
+    method:"POST",
+    // send the userId in the body as JSON
+    body:JSON.stringify({userId:userId})
+})
+.then(response => response.json())
+.then(data => console.log(data))
+
+
+fetch('http://127.0.0.1:8000/v2/visits?userId='+userId)
+.then(response => response.json())
+.then(data => console.log(data))
+
+fetch('http://127.0.0.1:8000/v2/visits?dbID='+dbID)
+.then(response => response.json())
+.then(data => console.log(data))
